93cf3d509e6978c3d05df1f2c8e0731e98099168
Raajzz <raajkumar.s2020@vitstudent.ac.in>
2021-12-26 23:15:04 +0530

    Added insert, need to pool and promisify
    Added insert demo statements without sanitizing, need to make con.query synchronous with promisify and need to start using pool rather than connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,40 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req,res)=>{
-  res.send("Hello World, yeah, made a change!!!");
-})
+const mysql = require("mysql2");
 
-app.listen(3001, ()=>{
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "fff_project",
+  password: "",
+});
+
+app.get("/", (req, res) => {
+  con.query(
+    'INSERT INTO userDetails VALUES("secondrow","https://www.npmjs.com/package/mysql2","M")',
+    (err) => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+  con.query(
+    'INSERT INTO userAbout VALUES("secondrow","Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius accusantium delectus nihil consequatur deleniti, velit saepe unde maxime veniam dolorum.")',
+    (err) => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+  con.query('INSERT INTO userFFF VALUES("secondrow",0,0,0)', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
+
+app.listen(3001, () => {
   console.log("running on port 3001");
-})
+});
 
